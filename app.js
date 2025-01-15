3b74fa1ff34e8cd25fca3c36a2f1505fd44df94c
HoeunRyu <hoeun9565@gmail.com>
2025-01-15 10:39:02 +0900

    feat/cors 설정

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 import express from "express";
 import mongoose from "mongoose";
-// import cors from "cors";
+import cors from "cors";
 import * as dotenv from "dotenv";
 import router from "./routes/index.js";
 
@@ -8,12 +8,12 @@ dotenv.config();
 
 const app = express();
 
-//TODO: 로컬호스트 주소, 배포한 주소 추가하기
-// const corsOptions = {
-//   origin: ["http://localhost:5000"],
-// };
+//로컬호스트 주소, 프론트 배포할 주소 추가하기
+const corsOptions = {
+  origin: ["http://localhost:3000", "https://pandamarket-hoeun.netlify.app/"],
+};
 
-// app.use(cors(corsOptions));
+app.use(cors(corsOptions));
 app.use(express.json()); //api 사용할 때 json형식을 사용할 수 있게 해줌
 
 //몽구스랑 연결

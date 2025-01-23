40e886539bb7ec0f8211542ad672c7f41c33c036
m-4verick <theodoreshin99@icloud.com>
2025-01-23 17:49:46 +0900

    df

--- src/services/productService.ts
+++ src/services/productService.ts
@@ -30,18 +30,19 @@ interface deleteProductService {
 }
 
 export const createProductService = ({title, description, price, tags}: createProductService) => {
-    const numPrice = Number(price);
     const data: {
         title: string,
         description: string,
-        price: number,
         tags?: string[],
-    } = {title, description, price: numPrice}
+    } = {title, description}
 
     if (tags) data.tags = tags;
 
     return prisma.product.create({
-        data: data,
+        data: {
+            ...data,
+            price: Number(price),
+        },
     })
 }
 

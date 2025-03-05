c86b9dc19f198425ab28c4dba7f1514a282c83c2
민지영 <kuck523@gmail.com>
2025-03-05 14:30:31 +0900

    ♻️ Update app.ts
    update app.ts

--- mission08/src/app.ts
+++ mission08/src/app.ts
@@ -7,14 +7,12 @@ import cors from "cors";
 dotenv.config();
 const app = express();
 
-console.log();
-
+// 모든 출처를 허용하거나, 필요한 출처만 허용할 수 있습니다.
 app.use(
   cors({
     origin: process.env.FRONTEND_URL, // 프론트엔드 URL 지정
     methods: ["GET", "POST", "PATCH", "DELETE"], // 허용할 HTTP 메서드
     allowedHeaders: ["Content-Type", "Authorization"], // 허용할 헤더
-    credentials: true, // 자격 증명 허용
   })
 );
 
@@ -24,5 +22,9 @@ app.use("/", router); // 라우터 세팅
 app.use((err: any, req: Request, res: Response, next: NextFunction) => {
   errorHandler(err, req, res, next);
 }); // 에러 처리
+
+app.listen(8000, () => {
+  console.log("Server is running on http://localhost:8000");
+});
 
 export default app;

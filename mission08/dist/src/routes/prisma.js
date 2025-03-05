0019ea9de5cfbe61ee3fbadc6089c5e17b9c6b06
민지영 <kuck523@gmail.com>
2025-03-05 14:17:13 +0900

    🔥 Remove dist
    remove dist

--- mission08/dist/src/routes/prisma.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const client_1 = require("@prisma/client");
-const extension_accelerate_1 = require("@prisma/extension-accelerate");
-const prisma = new client_1.PrismaClient().$extends((0, extension_accelerate_1.withAccelerate)());
-exports.default = prisma;
